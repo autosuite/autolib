01403cea41580bda9d43805fcdc713d3ef0cd5a8
Thomas Wang <thomas@teaminkling.com>
2020-06-01 00:08:13 +1000

    Change autolib replacement map from an interface to a class

--- src/index.ts
+++ src/index.ts
@@ -15,11 +15,17 @@ import * as core from '@actions/core';
 const SEMVER_REGEXP: RegExp = /v?(\d)\.(\d)\.\d)(.*)/;
 
 /** A [[RegExp]] to [[string]] replacement map for use on a file. */
-export interface ReplacementMap {
+export class ReplacementMap {
     /** The regular expression to match and replace. */
-    matcher: RegExp,
+    matcher: RegExp;
+
     /** The literal replacement to use to replace the given regular expression. */
-    replacement: string,
+    replacement: string;
+
+    constructor(matcher: RegExp, replacement: string) {
+        this.matcher = matcher;
+        this.replacement = replacement;
+    }
 };
 
 /** A basic concrete representation of a Semantic Version. */
