2ba947c08291c4fad295bc0eed29414d1570c2ac
Thomas Wang <thomas@teaminkling.com>
2020-06-25 02:37:23 +1000

    Fix major bug where SemVers with multiple digits weren't being read in

--- src/test/semver.test.ts
+++ src/test/semver.test.ts
@@ -12,20 +12,20 @@ describe("SemVer.constructFromText (nominal)", () => {
     each([
         /* SemVers with some extremes, some zeroes, and no info strings. */
 
-        ["0.10.5", 0, 10, 5, null],
-        ["912418.0.12419", 1, 3, 10, null],
-        ["00.00124.0124", 1, 3, 10, null],
-        ["3.31.12412", 1, 3, 10, null],
-        ["0.0.124192412", 1, 3, 10, null],
-        ["0.0.0", 1, 3, 10, null],
+        ["0.1.5", 0, 1, 5, null],
+        ["912418.0.12419", 912418, 0, 12419, null],
+        ["00.00124.0124", 0, 124, 124, null],
+        ["3.31.12412", 3, 31, 12412, null],
+        ["0.0.124192412", 0, 0, 124192412, null],
+        ["0.0.0", 0, 0, 0, null],
 
         /* SemVers with info strings. */
 
-        ["0.10.5-rc2", 0, 10, 5, "rc2"],
-        ["123.456.789-rc2+build2", 123, 456, 789, "rc2+build2"],
-        ["124124.1110.512-rc5+2020-06-25", 124124, 1110, 512, "rc5+2020-06-25"],
-        ["0001.10.22-invalid but supported", 1, 10, 22, "invalid but supported"],
-        ["0432.1098.5124-124uliuawnliu1248asd8(*!@Y*&", 432, 1098, 5124, "124uliuawnliu1248asd8(*!@Y*&"],
+        ["0.10.5-rc2", 0, 10, 5, "-rc2"],
+        ["123.456.789-rc2+build2", 123, 456, 789, "-rc2+build2"],
+        ["124124.1110.512-rc5+2020-06-25", 124124, 1110, 512, "-rc5+2020-06-25"],
+        ["0001.10.22-invalid but supported", 1, 10, 22, "-invalid but supported"],
+        ["0432.1098.5124-124uliuawnliu1248asd8(*!@Y*&", 432, 1098, 5124, "-124uliuawnliu1248asd8(*!@Y*&"],
     ]).it(
         "when the input is '%s'", (text, expectedMajor, expectedMinor, expectedPatch, expectedInfo) => {
             const version: SemVer = SemVer.constructFromText(text);
